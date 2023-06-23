5533c97ae2cde245124c37ba4e30e35b647d7d16
Ozod <bukhorizodao@gmail.com>
2023-06-23 21:05:39 +0500

    adding coffee cards and changing tabs with js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const buttons = document.querySelectorAll('#button-home, #button-menu, #button-contact');
+const homeBtn = document.querySelector('#button-home');
+const menuBtn = document.querySelector('#button-menu');
+const contactBtn = document.querySelector('#button-contact');
+const wholeContainer = document.querySelector('.whole-container');
+const wholeContainer2 = document.querySelector('.whole-container2');
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -9,6 +15,15 @@ buttons.forEach((button) => {
         button.classList.add('active');
     });
 });
+homeBtn.addEventListener('click', () => {
+    wholeContainer.style.display = 'flex';
+    wholeContainer2.style.display = 'none';
+});
+
+menuBtn.addEventListener('click', () => {
+    wholeContainer.style.display = 'none';
+    wholeContainer2.style.display = 'flex';
+})
 
 
   
